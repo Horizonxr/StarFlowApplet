2f682e8d3349dd4b68787e96eb8993359c8f0f02
Haliruya <60057614+Haliruya@users.noreply.github.com>
2021-11-12 23:37:15 +0800

    fix:修改输入框的bug，增添返回事件

--- joininProject.vue
+++ joininProject.vue
@@ -1,20 +1,16 @@
 <template name="joininProject">
-<view class="body">
-		
-			<view class="top">
-				<!-- 返回按钮 -->
-				<view class="top-button"><view class="iconfont icon-fanhui"></view></view>
-				<!-- 标题 -->
-				<view class="title">项目搜索</view>
-				<!-- 搜索栏 -->
-					<view class="search">
-						<input class="input-name" type="text" placeholder=" 输入项目名称进行查找" />
-						<view class="iconfont icon-sousuo"></view>
-				</view>
-			</view>
+	<view class="body2">
+		<view class="top-button2">
+			<view class="iconfont icon-fanhui" @click="back"></view>
+		</view>
+		<view class="title2">项目搜索</view>
+		<view class="search2">
+			<input class="input-name2" type="text" placeholder=" 输入项目名称进行查找" />
+			<view class="rectangle2"></view>
+			<view class="iconfont icon-sousuo" @click="search"></view>
+		</view>
 	</view>
 </template>
-
 <script>
 export default {
   name: 'myinput',
@@ -23,32 +19,34 @@ export default {
   },
   data() {
     return {
-                
+        flag:true 
     }
   },
   methods: {
-            
+    back(){
+		this.$emit("closeJoininpopup")
+	}, 
+	search(){
+		console.log("okkk")
+	}
   }
 }
 </script>
 
 <style lang="scss">
-.body{
-		height: 100vh;
-		background-color: $bg-color;
-		z-index: -999;
-	}
-.top{
+.body2{
+	height: 100vh;
 	position: relative;
 	width: 660rpx;
 	height: 1050rpx;
-	top:50rpx;
+	top:30rpx;
 	margin: 0 auto;
 	background-color: white;
 	border-radius: 30rpx;
 	box-shadow: 0 4rpx 12rpx #888888;
-	.top-button{
+	.top-button2{
 		position: absolute;
+		z-index: 100;
 		left:40rpx;
 		top:25rpx;
 		width: 86rpx;
@@ -57,7 +55,7 @@ export default {
 			font-size: 70rpx;
 			}
 	}
-	.title{
+	.title2{
 		position: relative;
 		top: 140rpx;
 		left: -65rpx;
@@ -67,29 +65,34 @@ export default {
 		line-height: 75rpx;
 		letter-spacing: 20rpx;
 	}
-	.search{
+	.search2{
 		position: relative;
 		top: 210rpx;
 		left: 50rpx;
 		font-size: 34rpx;
-		.input-name{
+		.rectangle2{
 			position: absolute;
-			top: 0rpx;
-			left: 0rpx;
-			font-size: 35rpx;
-			width: 550rpx;
-			letter-spacing: 10rpx;
-			height: 100rpx;
+			width: 570rpx;
+			left: -25rpx;
+			height: 80rpx;
 			border-radius: 15rpx;
 			border:1px solid #d2d2d2;
-			// background-color:transparent;
 		}
 		.iconfont{
 			position: absolute;
-			top:10rpx;
 			left: 450rpx;
 			font-size: 100rpx;
 		}
+		.input-name2{
+			position: absolute;
+			font-size: 35rpx;
+			width: 450rpx;
+			letter-spacing: 10rpx;
+			height: 100rpx;
+			border:none;
+		}
+		
+		
 	}
 }
 </style>
