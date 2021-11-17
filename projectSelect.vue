4a2e7068c6a3822fb1125f1b069c6d54107f24ff
Haliruya <1963709529@qq.com>
2021-11-17 23:33:26 +0800

    feat：增加了“查询加入项目请求的信息列表”的功能

--- projectSelect.vue
+++ projectSelect.vue
@@ -1,100 +1,204 @@
 <template>
 	<view class="body">
-		
+		<uni-popup class="morePopup" ref="morePopup" type="center" :mask-click="false">
+			<view class="wrapper">
+				<view class="botton-wrapper">
+					<view class="top-popup" @click="createPopup">
+						<view class="top-popup-text">创建项目</view>
+						<view>
+							<view class="iconfont icon-zengjia" ></view>
+						</view>
+					</view>	
+					<view class="middle-popup"  @click="joininPopup">
+					   <view class="middle-popup-text">加入项目</view>
+					   <view>
+						   <view class="iconfont icon-jiarubanji"></view>
+					   </view>
+					</view>
+					<view class="close-popup" @click="closePopup">
+						<view class="close-popup-text">取消</view>
+						<view>
+							<view class="iconfont icon-quxiao"@click=""></view>
+						</view>
+					</view>	
+				</view>
+			</view>
+		</uni-popup>
+		<!-- 第二层弹窗 -->
+		</uni-popup>
+		<uni-popup class="createPopup" ref="createPopup" type="center" :mask-click="false">
+			<createProject @closeCreatepopup="closeCreatepopup"></createProject>
+		</uni-popup>
+		<uni-popup class="joininPopup"  ref="joininPopup" type="center" :mask-click="false">
+			<joininProject  @closeJoininpopup="closeJoininpopup"></joininProject>
+		</uni-popup>
 		<view class="top-wrapper">
+			<!-- 顶部 -->
 			<view class="top">
 				<view class="title">项目选择</view>
-				<view class="account">当前账号：Horizon</view>
-				<navigator class="top-button" url="" hover-class="navigator-hover">
+				<view class="account">当前账号：{{userInfo.nickName}}</view>
+				<navigator class="top-button" url="/pages/accountSettings/accountSettings" hover-class="navigator-hover">
 				  <view class="iconfont icon-shezhi "></view>
 				</navigator>
 			</view>
 		</view>
-		
+		<!-- 按钮 -->
+		<view class="more" @click="openPopup">
+			<view class="iconfont icon-zengjia"></view>
+		</view>
+		<!-- 仓库列表 -->
 		<view class="list-wrapper">
-			<view class="list-item-wrapper">
-				<view class="list-item" v-for="(item, key) in mission_list" :key=item.key>
-					<view class="list-item-repositories">Horizon/TeamCase</view>
+			<view class="list-item-wrapper" v-for="(item, key) in project_list" :key=item.key>
+				<view class="list-item" @click="toProject(key)">
+					<view class="list-item-repositories">{{item.repo[0].fields.repo_name}}</view>
 					<view class="list-item-more">
-						<view class ="member-number">{{}}</view>
-						<view class="iconfont icon-ren111"></view>
-						<view class="list-item-description">This is a test case for TodoList</view>
-						<progress class="list-item-bar" stroke-width="20rpx" border-radius="300" active="true" percent="60"></progress>
+						<view class="member-number">
+							<view class="iconfont icon-ren111"></view>
+							<view class="number">15</view>
+						</view>
+						<view class="list-item-description">{{item.repo[0].fields.url}}</view>
+						<progress class="list-item-bar" duration=7 stroke-width="20rpx" border-radius="300" active="true" color= "#5091f2" :percent="item.repo[0].fields.finished * 100 / (item.repo[0].fields.checking+item.repo[0].fields.finished+item.repo[0].fields.incomplete)"></progress>
 					</view>
-					</view>
+				</view>
 			</view>
 		</view>
+		<!-- 背景 -->
 		<view class="bg-circle">
 			<view class='orange'></view>
 			<view class='blue'></view>
 			<view class='green'></view>
 		</view>
 		
-		<view class="more-add">
-			<view class="iconfont icon-zengjia"></view>
-		</view>
 	</view>
 </template>
-
 <script>
+	import {baseUrl} from '../../utils/config.js';
+	import createProject from '../../components/createProject';
+	import joininProject from '../../components/joininProject';
 	export default {
 		data() {
 			return {
-				mission_list:[1,2,3,4,5,6,7,9,10,11,22]
+				userInfo:[],
+				project_list:[],
+				u_id:-1
 			};
+		},
+		components:{createProject,joininProject},
+		methods:{
+			//弹窗相关方法
+			openPopup(){
+			  // 通过组件定义的ref调用uni-popup方法 ,如果传入参数 ，type 属性将失效 ，仅支持 ['top','left','bottom','right','center']
+				this.$refs.morePopup.open('center')
+			},
+			closePopup(){
+				this.$refs.morePopup.close()
+			},
+			createPopup(){
+				this.$refs.createPopup.open('center')
+			 },
+			closeCreatepopup(){
+				this.$refs.createPopup.close()
+			},
+			joininPopup(){
+				this.$refs.joininPopup.open('center')
+			},
+			closeJoininpopup(){
+				this.$refs.joininPopup.close()
+			},
+			toProject(key){
+				console.log(this.project_list[key])
+				let repo = this.project_list[key].repo[0].pk
+				let repo_name = this.project_list[key].repo[0].fields.repo_name
+				uni.navigateTo({
+					url:'../projectList/projectList?repo_id=' + repo + '&' + 'repo_name=' + repo_name,
+					animationDuration:300
+				})
+			}
+		},
+		onLoad() {
+			uni.showLoading({
+				title:"加载中",
+				mask:true
+			})
+			this.u_id = uni.getStorageSync("u_id")
+			this.userInfo = uni.getStorageSync("userInfo")
+			uni.request({
+			    url: baseUrl + '/repo/showRepo', //仅为示例，并非真实接口地址。
+				method:'POST',
+				timeout:2000,
+			    data: {
+			        u_id: this.u_id
+			    },
+			    header: {
+			        "content-type": "application/x-www-form-urlencoded" //自定义请求头信息
+			    },
+			    success: (res) => {
+					console.log(res.data)
+					this.project_list = res.data.data
+					uni.hideLoading()
+			    },
+				fail() {
+					uni.hideLoading()
+					uni.showToast({
+						title: '请求失败',
+						icon:'error'
+					});
+				}
+			})
+
 		}
 	}
 </script>
 
 <style lang="scss">
 	.body{
-		height: 100vh;
-		// background-color: $bg-color;
-		z-index: -999;
+		
 	}
-	.bg-circle{
-		.orange{
-			width: 90px;
-			height: 90px;
-			border-radius: 50%;
-			position: fixed;
-			z-index: 0;
-			left: 80rpx;
-			top: 450rpx;
-			background: rgba($color: $uni-color-warning, $alpha: 0.9);			
-		}
-		.blue{
-			width: 180px;
-			height: 180px;
-			left: 230rpx;
-			top: 400rpx;
-			position: fixed;
-			border-radius: 50%;
-			background: rgba($color: $pending-mission, $alpha: 0.9);
-		}
-		.green{
-			width: 120px;
-			height: 120px;
-			left: 190rpx;
-			top: 700rpx;
-			position: fixed;
-			border-radius: 50%;
-			background: rgba($color: $uni-color-success, $alpha: 0.9);
+	.morePopup{
+		z-index: 15;
+		margin: 0 auto;
+		.wrapper{
+			width: 100%;
+			height: 100vh;
+			.botton-wrapper{
+				position: absolute;
+				height: 600rpx;
+				width: 364rpx;
+				bottom: 59rpx;
+				left: -23rpx;
+				display: flex;
+				flex-direction: column;
+				view{
+					width: 364rpx;
+					height: 200rpx;
+					display: flex;
+					justify-content: center;
+					view:nth-child(1){
+						width: 210rpx;
+						height: 200rpx;
+						font-size: 40rpx;
+						line-height: 200rpx;
+						color: white;
+					}
+					view:nth-child(2){
+						width: 130rpx;
+						height: 130rpx;
+						margin-top: 41rpx;
+						background-color: white;
+						border-radius: 50%;
+						display: flex;
+						box-shadow: 0 4rpx 12rpx #888888;
+						.iconfont{
+							line-height: 130rpx;
+							text-align: center;
+							font-size: 100rpx;
+							color:black;
+						}
+					}
+				}
+			}
 		}
 	}
-	.more-add{
-		position: fixed;		
-	 	.iconfont{z-index: 100;
-	 	position: fixed;
-	 	font-size: 120rpx;
-	 	left: 560rpx;
-	 	top: 1000rpx;
-	 	border-radius: 50%;
-	 	background: rgba($color:#fff, $alpha: 1);	
-	 	box-shadow: 15rpx 17rpx 15rpx 2rpx #888888;	
-	}
-}
-	
 	.top-wrapper{
 		top:0rpx;
 		position: sticky;
@@ -145,11 +249,9 @@
 		width: 100%;
 		height: 100%;
 		z-index: 1;
-		// background-color: $bg-color;
 		.list-item-wrapper{
 			margin-top: 30rpx;
 			width: 100%;
-			// background-color: $bg-color;
 			.list-item{
 				margin: 22rpx auto;
 				height: 120rpx;
@@ -172,20 +274,26 @@
 				}
 				.member-number{
 					position: relative;
-					left:50rpx;
-					top:-10rpx;
-				}
-				.iconfont{
-					position: relative;
 					left:500rpx;
 					top:-20rpx;
-					font-size: 50rpx;
+					width: 200rpx;
+					height: 50rpx;
+					.iconfont{
+						font-size: 50rpx;
+					}
+					.number{
+						position: relative;
+						font-size: 30rpx;
+						top:-45rpx;
+						right: -57rpx;
+					}
 				}
+
 				
 				.list-item-description{
 					position: relative;
 					left: 21rpx;
-					top:-30rpx;
+					top:-24rpx;
 					height: 25rpx;
 					font-size: 25rpx;
 					line-height: 25rpx;
@@ -193,13 +301,57 @@
 				}
 				.list-item-bar{
 					position: relative;
-					left: 21rpx;
 					top:-10rpx;
-					stroke-width:6rpx ;
-					font-size: 20rpx;
+					width: 570rpx;
+					left: 10rpx;
 				}
 			}
 		}
 	}
-	
+	.bg-circle{
+		z-index: 0;
+		.orange{
+			width: 90px;
+			height: 90px;
+			border-radius: 50%;
+			position: fixed;
+			left: 80rpx;
+			top: 450rpx;
+			background: rgba($color: $uni-color-warning, $alpha: 0.9);			
+		}
+		.blue{
+			width: 180px;
+			height: 180px;
+			left: 230rpx;
+			top: 400rpx;
+			position: fixed;
+			border-radius: 50%;
+			background: rgba($color: $pending-mission, $alpha: 0.9);
+		}
+		.green{
+			width: 120px;
+			height: 120px;
+			left: 190rpx;
+			top: 700rpx;
+			position: fixed;
+			border-radius: 50%;
+			background: rgba($color: $uni-color-success, $alpha: 0.9);
+		}
+	}
+	.more{
+		position: fixed;
+		width: 130rpx;
+		height: 130rpx;
+		right: 46rpx;
+		bottom: 90rpx;
+		border-radius: 50%;
+		background-color: white;
+		z-index: 10;
+		text-align: center;
+		line-height: 130rpx;
+		box-shadow: 0 4rpx 12rpx #888888;
+		.iconfont{
+			font-size: 130rpx;
+		}
+	}
 </style>
