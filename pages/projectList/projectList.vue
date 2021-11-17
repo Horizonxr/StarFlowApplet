76c93ab2c6e0adc7465b2d868d721d3c71eaa01a
Horizonxr <1176542271@qq.com>
2021-11-17 18:30:55 +0800

    fearture:完成任务列表展示业务逻辑 style:将顶部导航栏改为白色

--- pages/projectList/projectList.vue
+++ pages/projectList/projectList.vue
@@ -28,7 +28,7 @@
 		<view class="top-wrapper">
 			<view class="top">
 				<view class="title">项目名称</view>
-				<view class="account">仓库：{{}}</view>
+				<view class="account">仓库：{{repo_name}}</view>
 				<view class="top-button">
 					<view class="iconfont icon-shezhi" @click="myPopup"></view>
 				</view>
@@ -36,9 +36,23 @@
 		</view>
 		<view class="list-wrapper">
 			<view class="list-item-wrapper">
-				<view class="list-item" v-for="(item, key) in mission_list" :key=item.key>
-					<view class="list-item-mission">任务：完成XXX特性</view>
-					<view class="list-item-DDL">DeadLine: 2021.11.06</view>
+				<view class="list-item" style="background-color: #8feb9b;" v-for="(item, key) in mission_list.incomplete" :key=item.key>
+					<view class="list-item-mission">任务：{{item.task_name}}</view>
+					<view class="list-item-DDL">DeadLine: {{DDLcompute(item.deadline)}}</view>
+					<view class="list-item-more">
+						<view class="iconfont icon-gengduo"></view>
+					</view>
+				</view>
+				<view class="list-item" style="background-color: #7fa9f2;" v-for="(item, key) in mission_list.checking" :key=item.key>
+					<view class="list-item-mission">任务：{{item.task_name}}</view>
+					<view class="list-item-DDL">DeadLine: {{DDLcompute(item.deadline)}}</view>
+					<view class="list-item-more">
+						<view class="iconfont icon-gengduo"></view>
+					</view>
+				</view>
+				<view class="list-item" style="background-color: #a6a5a5;" v-for="(item, key) in mission_list.finish" :key=item.key>
+					<view class="list-item-mission">任务：{{item.task_name}}</view>
+					<view class="list-item-DDL">DeadLine: {{DDLcompute(item.deadline)}}</view>
 					<view class="list-item-more">
 						<view class="iconfont icon-gengduo"></view>
 					</view>
@@ -68,7 +82,13 @@
 	export default {
 		data() {
 			return {
-				mission_list: [1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 22],
+				repo_name:'',
+				repo_id:-1,
+				mission_list: {
+					incomplete:[],
+					checking:[],
+					finish:[]
+				}
 			};
 		},
 		methods: {
@@ -89,9 +109,48 @@
 			},
 			closemyPopup() {
 				this.$refs.myproject.close("center")
+			},
+			DDLcompute(DDL){
+				let DDLjoin = DDL[0].toString()
+				for (let i = 1;i<3;i++){
+					DDLjoin = DDLjoin + '.' +DDL[i].toString()
+				}
+				return DDLjoin
 			}
 
-
+		},
+		onLoad(option) { //option为object类型，会序列化上个页面传递的参数
+		    console.log(option);
+			uni.showLoading({
+				title:'加载中'
+			})
+			this.repo_name = option.repo_name
+			this.repo_id = option.repo_id
+			uni.request({
+			    url: baseUrl + '/repo/showTask', //仅为示例，并非真实接口地址。
+				method:'POST',
+				timeout:2000,
+			    data: {
+			        repo_id: option.repo_id
+			    },
+			    header: {
+			        "content-type": "application/x-www-form-urlencoded" //自定义请求头信息
+			    },
+			    success: (res) => {
+					console.log(res.data)
+					this.mission_list.incomplete = res.data.incomplete
+					this.mission_list.checking = res.data.checking
+					this.mission_list.finish = res.data.finish
+					uni.hideLoading()
+			    },
+				fail() {
+					uni.hideLoading()
+					uni.showToast({
+						title: '请求失败',
+						icon:'error'
+					});
+				}
+			})
 		}
 
 
@@ -139,6 +198,7 @@
 					bottom: 424rpx;
 					right: 46rpx;
 					background-color: #FFF;
+					box-shadow: 0 4rpx 12rpx #888888;
 				}
 
 				.help-text {
@@ -161,7 +221,7 @@
 					bottom: 255rpx;
 					right: 46rpx;
 					background-color: #FFF;
-
+					box-shadow: 0 4rpx 12rpx #888888;
 
 				}
 
@@ -184,7 +244,7 @@
 					bottom: 90rpx;
 					right: 46rpx;
 					background-color: #FFF;
-
+					box-shadow: 0 4rpx 12rpx #888888;
 				}
 
 			}
@@ -317,6 +377,7 @@
 				position: absolute;
 				left: 567rpx;
 				background-color: #FFF;
+				box-shadow: 0 4rpx 12rpx #888888;
 			}
 
 			.progress-box {
