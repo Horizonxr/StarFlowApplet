592c33d626bc9f05fa0581958c8a0793da6b0a7a
lkdj <60039363+lkdj@users.noreply.github.com>
2021-11-18 22:38:32 +0800

    feat:实现创建项目

--- components/createProject.vue
+++ components/createProject.vue
@@ -7,9 +7,16 @@
 		<!-- 搜索 -->
 		<view class="search1">
 			<view class="repositories-list1">仓库列表</view>
-			<input class="input-name1" type="text" placeholder=" 输入仓库名称进行检索" />
+			<input class="input-name1" type="text"@input="input":value=keyword placeholder=" 输入仓库名称进行检索" />
 			<view class="rectangle1"></view>
 			<view class="iconfont icon-sousuo" @click="search"></view>
+			<view class="list-item1" v-for="(item, key) in repositories_list" :key=item.key>
+			<view class="list-item-repositories1" @click="openPopup">{{item.repo_name}}</view>
+			<!-- 加入项目提示信息 -->
+			<uni-popup ref="popup" type="dialog">
+			    <uni-popup-dialog type='info' title="提示"mode="base" content="确认创建该项目？"message="成功消息" :duration="2000" :before-close="true" @close="close" @confirm="request_joinin(key)"></uni-popup-dialog>
+			</uni-popup>
+		</view>
 		</view>
 		<!-- 确认按钮 -->
 		<view class="bottom-button1">
@@ -19,26 +26,103 @@
 </template>
 
 <script>
+	import {baseUrl} from '../utils/config.js';
 export default {
-  name: 'myinput',
-  props: {
-            
-  },
-  data() {
-    return {
-                
-    }
-  },
-  methods: {
-          back(){
-          	this.$emit("closeCreatepopup");
-          },  
-          search(){
-          	
-          },
-          finish(){
-          	
-          }  
+    name: 'myinput',
+	props: {},
+	data() {
+		return {
+            u_id:1,
+            repositories_list:[],
+            keyword:''     
+		}
+	},
+    methods: {
+        close(){
+			this.$refs.popup[0].close()
+        },  
+        request_joinin(key){
+         	this.$refs.popup[0].close()
+         	console.log("ke")
+         	uni.reLaunch({
+         		url: '/pages/projectList/projectList'
+         	})
+        },
+        openPopup(){
+           // 通过组件定义的ref调用uni-popup方法 ,如果传入参数 ，type 属性将失效 ，仅支持 ['top','left','bottom','right','center']
+        	this.$refs.popup[0].open('center')
+        },
+        back(){
+        	this.keyword=''
+        	// this.repositories_list=[]
+           	this.$emit("closeCreatepopup");
+        }, 
+        input(e){
+        	this.keyword=e.target.value
+        },
+		search(){
+			console.log(this.keyword)
+			uni.showLoading({
+				title:"加载中",
+				mask:true
+			})	
+			uni.request({
+			    url: baseUrl + '/repo/getReposByKeyword', //仅为示例，并非真实接口地址。
+				method:'POST',
+				timeout:8000,
+			    data: {
+			        u_id:this.u_id,
+					keyword:this.keyword
+			    },
+			    header: {
+			        "content-type": "application/x-www-form-urlencoded" //自定义请求头信息
+			    },
+			    success: (res) => {
+					this.repositories_list = res.data.data
+					uni.hideLoading()
+			    },
+				fail() {
+					uni.hideLoading()
+					uni.showToast({
+						title: '请求失败',
+						icon:'error'
+					});
+				}
+			})	
+		},
+	},
+		mounted:function() {
+			this.$nextTick(function () {
+			// 仅在整个视图都被渲染之后才会运行的代码
+				uni.showLoading({
+					title:"加载中",
+					mask:true
+				})
+				// this.u_id = uni.getStorageSync("u_id")
+				console.log(this.u_id)
+				uni.request({
+					url: baseUrl + '/repo/getRepos', //仅为示例，并非真实接口地址。
+					method:'POST',
+					timeout:8000,
+						data: {
+							u_id: this.u_id
+						},
+					header: {
+						"content-type": "application/x-www-form-urlencoded" //自定义请求头信息
+					},
+					success: (res) => {
+						this.repositories_list = res.data.data
+						uni.hideLoading()
+					},
+					fail() {
+						uni.hideLoading()
+						uni.showToast({
+							title: '请求失败',
+							icon:'error'
+						});
+					}
+				})
+			})
   }
 }
 </script>
@@ -113,6 +197,20 @@ export default {
 			top: 1rpx;
 			left: 525rpx;
 			font-size: 64rpx;
+		}
+		.list-item1{
+			margin: 25rpx 20rpx auto;
+			position: relative;
+			top:40rpx;
+			left:-10rpx;
+			height: 60rpx;
+			width: 500rpx;
+			border-radius: 10rpx;
+			box-shadow: 0 4rpx 12rpx #888888;
+			background-color:rgba($color: #ffff, $alpha: 0.5);
+			.list-item-repositories1{
+				text-align: center;
+			}
 		}
 	}
 	.bottom-button1{
