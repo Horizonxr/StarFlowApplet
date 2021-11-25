dd4651b23c56192414a586bd13d455178657f27d
瀚垠 <547446767@qq.com>
2021-11-25 09:25:10 +0800

    feature：点按项目设置进行跳转

--- pages/welcom/welcom.vue
+++ pages/welcom/welcom.vue
@@ -3,7 +3,7 @@
 		<view>欢迎使用星流小程序</view>
 		<view>小程序是一款辅助GitHub进行任务管理的TodoList</view>
 		<view>在开始使用前，先让我们进行一些简单的设置</view>
-		<view>转到设置</view>
+		<view @click="toAccountSettings()">转到设置</view>
 	</view>
 </template>
 
@@ -15,7 +15,11 @@
 			};
 		},
 		methods:{
-			
+			toAccountSettings(){
+				uni.redirectTo({
+					url:'../accountSettings/accountSettings'
+				})
+			}
 		},
 		onLoad() {
 			this.is_first = uni.getStorageSync('is_first')
