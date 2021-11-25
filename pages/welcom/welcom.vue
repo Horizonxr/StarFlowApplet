79880ece27d24cdf39d73f4bb27793697164be3f
瀚垠 <547446767@qq.com>
2021-11-25 09:11:42 +0800

    create：新增欢迎页

---
+++ pages/welcom/welcom.vue
@@ -0,0 +1,24 @@
+<template>
+	<view>
+		<view>欢迎使用星流小程序</view>
+		<view>小程序是一款辅助GitHub进行任务管理的TodoList</view>
+		<view>在开始使用前，先让我们进行一些简单的设置</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				is_first:1
+			};
+		},
+		methods:{
+			
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
