22c3d2af6242a4815ce188c9f4eb3f22c0bae149
瀚垠 <547446767@qq.com>
2021-11-25 09:53:32 +0800

    bugFix：修正了页面无法正确判断是否第一次登陆的bug feature：将第一页设置为启动页

--- pages/welcom/welcom.vue
+++ pages/welcom/welcom.vue
@@ -1,8 +1,10 @@
 <template>
 	<view>
-		<view>欢迎使用星流小程序</view>
-		<view>小程序是一款辅助GitHub进行任务管理的TodoList</view>
-		<view>在开始使用前，先让我们进行一些简单的设置</view>
+		<view class="tips-wrapper">
+			<view>欢迎使用星流小程序</view>
+			<view>这是一款辅助GitHub进行任务管理的TodoList小程序</view>
+			<view>在开始使用前，先让我们进行一些简单的设置</view>
+		</view>
 		<view @click="toAccountSettings()">转到设置</view>
 	</view>
 </template>
@@ -22,18 +24,15 @@
 			}
 		},
 		onLoad() {
-			this.is_first = uni.getStorageSync('is_first')
-			if (!this.is_first){
+			if (uni.getStorageSync('is_first')==0){
 				uni.redirectTo({
 					url:'../projectSelect/projectSelect'
 				})
 			}
-			else {
-				uni.setStorage({
-					key:'is_first',
-					data:1
-				})
-			}
+			uni.setStorage({
+				key:'is_first',
+				data:0
+			})
 		}
 	}
 </script>
