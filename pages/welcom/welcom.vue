d88d5634ad0b3e555fd6f52f7822f57215d95e34
瀚垠 <547446767@qq.com>
2021-11-25 09:21:56 +0800

    feature：实现欢迎页自动跳转

--- pages/welcom/welcom.vue
+++ pages/welcom/welcom.vue
@@ -3,6 +3,7 @@
 		<view>欢迎使用星流小程序</view>
 		<view>小程序是一款辅助GitHub进行任务管理的TodoList</view>
 		<view>在开始使用前，先让我们进行一些简单的设置</view>
+		<view>转到设置</view>
 	</view>
 </template>
 
@@ -15,6 +16,20 @@
 		},
 		methods:{
 			
+		},
+		onLoad() {
+			this.is_first = uni.getStorageSync('is_first')
+			if (!this.is_first){
+				uni.redirectTo({
+					url:'../projectSelect/projectSelect'
+				})
+			}
+			else {
+				uni.setStorage({
+					key:'is_first',
+					data:1
+				})
+			}
 		}
 	}
 </script>
